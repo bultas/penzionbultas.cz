28120701cb45b4d0cb20097cf4aff03134e48e87
Daniel Bultas <bultas@Daniel-MacBook-Air.local>
2015-04-14 11:06:28 +0200

    add website files

---
+++ wp-content/plugins/nextgen-gallery/products/photocrati_nextgen/modules/ngglegacy/xml/media-rss.php
@@ -0,0 +1,131 @@
+<?xml version='1.0' encoding='UTF-8' standalone='yes'?>
+<rss version='2.0' xmlns:media='http://search.yahoo.com/mrss/' xmlns:atom='http://www.w3.org/2005/Atom'>
+	<channel>
+		<generator><![CDATA[NextGEN Gallery [http://nextgen-gallery.com]]]></generator>
+		<title>Penzion Bultas</title>
+		<description>Unterkunft Böhmerwald - Restaurant Markt Eisenstein</description>
+		<link><![CDATA[http://www.penzionbultas.cz]]></link>
+		<atom:link rel='self' href='http://www.penzionbultas.cz/wp-content/plugins/nextgen-gallery/products/photocrati_nextgen/modules/ngglegacy/xml/media-rss.php' type='application/rss+xml' />
+		<atom:link rel='next' href='http://www.penzionbultas.cz/wp-content/plugins/nextgen-gallery/products/photocrati_nextgen/modules/ngglegacy/xml/media-rss.php?show=10&amp;page=1&amp;mode=last_pictures' />
+		<item>
+			<title><![CDATA[uvod]]></title>
+			<description><![CDATA[ ]]></description>
+			<link><![CDATA[http://www.penzionbultas.cz/wp-content/gallery/galerie/uvod.jpg]]></link>
+			<guid>image-id:102</guid>
+			<media:content url='http://www.penzionbultas.cz/wp-content/gallery/galerie/uvod.jpg' medium='image' />
+			<media:title><![CDATA[uvod]]></media:title>
+			<media:description><![CDATA[ ]]></media:description>
+			<media:thumbnail url='http://www.penzionbultas.cz/wp-content/gallery/galerie/thumbs/thumbs_uvod.jpg' width='80' height='80' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Penzion Bultas (http://www.penzionbultas.cz)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[imgp9792]]></title>
+			<description><![CDATA[ ]]></description>
+			<link><![CDATA[http://www.penzionbultas.cz/wp-content/gallery/galerie/imgp9792.jpg]]></link>
+			<guid>image-id:101</guid>
+			<media:content url='http://www.penzionbultas.cz/wp-content/gallery/galerie/imgp9792.jpg' medium='image' />
+			<media:title><![CDATA[imgp9792]]></media:title>
+			<media:description><![CDATA[ ]]></media:description>
+			<media:thumbnail url='http://www.penzionbultas.cz/wp-content/gallery/galerie/thumbs/thumbs_imgp9792.jpg' width='80' height='80' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Penzion Bultas (http://www.penzionbultas.cz)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[imgp9784]]></title>
+			<description><![CDATA[ ]]></description>
+			<link><![CDATA[http://www.penzionbultas.cz/wp-content/gallery/galerie/imgp9784.jpg]]></link>
+			<guid>image-id:100</guid>
+			<media:content url='http://www.penzionbultas.cz/wp-content/gallery/galerie/imgp9784.jpg' medium='image' />
+			<media:title><![CDATA[imgp9784]]></media:title>
+			<media:description><![CDATA[ ]]></media:description>
+			<media:thumbnail url='http://www.penzionbultas.cz/wp-content/gallery/galerie/thumbs/thumbs_imgp9784.jpg' width='80' height='80' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Penzion Bultas (http://www.penzionbultas.cz)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[imgp9776]]></title>
+			<description><![CDATA[ ]]></description>
+			<link><![CDATA[http://www.penzionbultas.cz/wp-content/gallery/galerie/imgp9776.jpg]]></link>
+			<guid>image-id:99</guid>
+			<media:content url='http://www.penzionbultas.cz/wp-content/gallery/galerie/imgp9776.jpg' medium='image' />
+			<media:title><![CDATA[imgp9776]]></media:title>
+			<media:description><![CDATA[ ]]></media:description>
+			<media:thumbnail url='http://www.penzionbultas.cz/wp-content/gallery/galerie/thumbs/thumbs_imgp9776.jpg' width='80' height='80' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Penzion Bultas (http://www.penzionbultas.cz)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[OLYMPUS DIGITAL CAMERA]]></title>
+			<description><![CDATA[ ]]></description>
+			<link><![CDATA[http://www.penzionbultas.cz/wp-content/gallery/galerie/p8292818.jpg]]></link>
+			<guid>image-id:97</guid>
+			<media:content url='http://www.penzionbultas.cz/wp-content/gallery/galerie/p8292818.jpg' medium='image' />
+			<media:title><![CDATA[OLYMPUS DIGITAL CAMERA]]></media:title>
+			<media:description><![CDATA[ ]]></media:description>
+			<media:thumbnail url='http://www.penzionbultas.cz/wp-content/gallery/galerie/thumbs/thumbs_p8292818.jpg' width='80' height='80' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Penzion Bultas (http://www.penzionbultas.cz)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[OLYMPUS DIGITAL CAMERA]]></title>
+			<description><![CDATA[ ]]></description>
+			<link><![CDATA[http://www.penzionbultas.cz/wp-content/gallery/galerie/p8292814.jpg]]></link>
+			<guid>image-id:96</guid>
+			<media:content url='http://www.penzionbultas.cz/wp-content/gallery/galerie/p8292814.jpg' medium='image' />
+			<media:title><![CDATA[OLYMPUS DIGITAL CAMERA]]></media:title>
+			<media:description><![CDATA[ ]]></media:description>
+			<media:thumbnail url='http://www.penzionbultas.cz/wp-content/gallery/galerie/thumbs/thumbs_p8292814.jpg' width='80' height='80' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Penzion Bultas (http://www.penzionbultas.cz)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[OLYMPUS DIGITAL CAMERA]]></title>
+			<description><![CDATA[ ]]></description>
+			<link><![CDATA[http://www.penzionbultas.cz/wp-content/gallery/galerie/p8292813.jpg]]></link>
+			<guid>image-id:95</guid>
+			<media:content url='http://www.penzionbultas.cz/wp-content/gallery/galerie/p8292813.jpg' medium='image' />
+			<media:title><![CDATA[OLYMPUS DIGITAL CAMERA]]></media:title>
+			<media:description><![CDATA[ ]]></media:description>
+			<media:thumbnail url='http://www.penzionbultas.cz/wp-content/gallery/galerie/thumbs/thumbs_p8292813.jpg' width='80' height='80' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Penzion Bultas (http://www.penzionbultas.cz)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[OLYMPUS DIGITAL CAMERA]]></title>
+			<description><![CDATA[ ]]></description>
+			<link><![CDATA[http://www.penzionbultas.cz/wp-content/gallery/galerie/p1314328.jpg]]></link>
+			<guid>image-id:94</guid>
+			<media:content url='http://www.penzionbultas.cz/wp-content/gallery/galerie/p1314328.jpg' medium='image' />
+			<media:title><![CDATA[OLYMPUS DIGITAL CAMERA]]></media:title>
+			<media:description><![CDATA[ ]]></media:description>
+			<media:thumbnail url='http://www.penzionbultas.cz/wp-content/gallery/galerie/thumbs/thumbs_p1314328.jpg' width='80' height='80' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Penzion Bultas (http://www.penzionbultas.cz)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[imgp9843]]></title>
+			<description><![CDATA[ ]]></description>
+			<link><![CDATA[http://www.penzionbultas.cz/wp-content/gallery/galerie/imgp9843.jpg]]></link>
+			<guid>image-id:93</guid>
+			<media:content url='http://www.penzionbultas.cz/wp-content/gallery/galerie/imgp9843.jpg' medium='image' />
+			<media:title><![CDATA[imgp9843]]></media:title>
+			<media:description><![CDATA[ ]]></media:description>
+			<media:thumbnail url='http://www.penzionbultas.cz/wp-content/gallery/galerie/thumbs/thumbs_imgp9843.jpg' width='80' height='80' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Penzion Bultas (http://www.penzionbultas.cz)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[imgp9838]]></title>
+			<description><![CDATA[ ]]></description>
+			<link><![CDATA[http://www.penzionbultas.cz/wp-content/gallery/galerie/imgp9838.jpg]]></link>
+			<guid>image-id:92</guid>
+			<media:content url='http://www.penzionbultas.cz/wp-content/gallery/galerie/imgp9838.jpg' medium='image' />
+			<media:title><![CDATA[imgp9838]]></media:title>
+			<media:description><![CDATA[ ]]></media:description>
+			<media:thumbnail url='http://www.penzionbultas.cz/wp-content/gallery/galerie/thumbs/thumbs_imgp9838.jpg' width='80' height='80' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Penzion Bultas (http://www.penzionbultas.cz)]]></media:copyright>
+		</item>
+	</channel>
+</rss>
---
+++ wp-content/themes/TealGray.v2/js/functions.js
@@ -0,0 +1,47 @@
+$(function(){
+	
+
+	$('#gallery').gallerify();
+	
+	// easter egg :)
+	$('<div id="worm"></div>').appendTo('#footer_sleeve').toggle(
+		function(){
+			$(this).parents('#footer_sleeve').animate({height:'835px'},{queue:false,duration:2000});
+			$('html,body').animate({scrollTop:$('#footer_sleeve').offset().top + 865},{queue:false,duration:2000});
+		}, function(){
+			$(this).parents('#footer_sleeve').animate({height:'70px'},{queue:false,duration:2000});
+		});
+});
+
+	jQuery.fn.gallerify = function() {
+		return this.each(function(){
+			var images = $('img.thumb',this);
+			images.hide().css({opacity:0});
+			$(images[0]).show().css({opacity:1});
+			
+			if (images.length > 1) {
+				$(this).after('<ul id="gallery_changer"></ul>');
+				var changer = $('+ul',this);
+				images.each(function(){
+					var numberLink = (images.index(this)+1).toString();
+					if (numberLink.length == 1) numberLink = '0' + numberLink;
+					$('<li><a href="#">'+numberLink+'</a></li>').click(showImage).appendTo(changer);
+				});
+				$('li:first',changer).addClass('first current');
+			}
+
+			function showImage() {
+				$(this).addClass('current').siblings().removeClass('current');
+				var clicked = this;
+				images.each(function(){
+					if ($(this).is(':visible')) {
+						$(this).animate({opacity:0},200,function(){
+							$(this).hide();
+							$(images[$(clicked).parent().children('li').index(clicked)]).css({display:'block'}).animate({opacity:1},200);
+						});
+					}
+				});
+				return false;
+			}
+		});
+	}
